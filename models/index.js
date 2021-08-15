4ec2eb41927bfafc3293a860ae706e399cdee13a
patexo <patexowin@hotmail.es>
2021-08-15 21:14:55 +0200

    Step 1.3.c: Heroku pool

--- models/index.js
+++ models/index.js
@@ -3,13 +3,21 @@ const path = require('path');
 // Load ORM
 const Sequelize = require('sequelize');
 
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 // Environment variable to define the URL of the data base to use.
 // To use SQLite data base:
 //    DATABASE_URL = sqlite:sticker.sqlite
 // To use  Heroku Postgres data base:
 //    DATABASE_URL = postgres://user:passwd@host:port/database
-const url = process.env.DATABASE_URL || "sqlite:sticker.sqlite";
+// const url = process.env.DATABASE_URL || "sqlite:sticker.sqlite";
+const url = pool.connectionString || "sqlite:sticker.sqlite";
 
 const sequelize = new Sequelize(url);
 
