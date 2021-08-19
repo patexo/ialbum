6b4115cab3a9479e214dffb57166e6501f73c8ec
patexo <patexowin@hotmail.es>
2021-08-19 18:20:16 +0200

    Step 1.4.2: Cromos findall

--- controllers/cromo.js
+++ controllers/cromo.js
@@ -1,11 +1,20 @@
 const Sequelize = require("sequelize");
 const {models} = require("../models");
 
+const { Pool } = require ('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 // Autoload el cromo asociado a :cromoId
 exports.load = async (req, res, next, cromoId) => {
 
     try {
-        const cromo = await models.Sticker.findByPk(cromoId);
+        const client = await pool.connect();
+        const cromo = await client.query.findByPk(cromoId);
         if (cromo) {
             req.load = {...req.load, cromo};
             next();
@@ -22,9 +31,12 @@ exports.load = async (req, res, next, cromoId) => {
 exports.index = async (req, res, next) => {
 
     try {
-        const cromos = await models.Sticker.findAll();
+        const client = await pool.connect();
+        const cromos = await client.query.findAll();
         res.render('cromos/index.ejs', {cromos});
+        client.release();
     } catch (error) {
         next(error);
     }
 };
+
